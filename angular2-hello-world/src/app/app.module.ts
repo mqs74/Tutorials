f6282ecde08b1dff231d2ef8517f321c69762ad8
OFM <ofm@ofm.clear2pay.com>
2017-08-29 15:22:56 +0200

    New modules

--- angular2-hello-world/src/app/app.module.ts
+++ angular2-hello-world/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
 import { HelloWordComponent } from './hello-word/hello-word.component';
+import { UserItemComponent } from './user-item/user-item.component';
+import { UserListComponent } from './user-list/user-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloWordComponent
+    HelloWordComponent,
+    UserItemComponent,
+    UserListComponent
   ],
   imports: [
     BrowserModule,

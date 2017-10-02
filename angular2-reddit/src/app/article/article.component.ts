280aba94a74ae6e7c8ec3a3f20584f366cd74f61
OFM <ofm@ofm.clear2pay.com>
2017-10-02 15:17:16 +0200

    Tutorial progress

--- angular2-reddit/src/app/article/article.component.ts
+++ angular2-reddit/src/app/article/article.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Article } from './article.model';
 
 @Component({
   selector: 'app-article',
   templateUrl: './article.component.html',
-  styleUrls: ['./article.component.css']
+  styleUrls: ['./article.component.css'],
+  host: {
+    class: 'row'
+  }
 })
 export class ArticleComponent implements OnInit {
-
-  constructor() { }
+  @Input() article: Article;
 
   ngOnInit() {
   }
 
+  voteUp(): boolean {
+    this.article.voteUp();
+    return false;
+  }
+
+  voteDown(): boolean {
+    this.article.voteDown();
+    return false;
+  }
 }
--- spring-data-tutorial/paging-and-sorting-init/src/main/java/com/owczarek/RunAtStart.java
+++ spring-data-tutorial/paging-and-sorting-init/src/main/java/com/owczarek/RunAtStart.java
@@ -1,12 +1,19 @@
 package com.owczarek;
 
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
+import java.util.List;
 
 @Component
 public class RunAtStart {
+    private Logger logger = Logger.getLogger(RunAtStart.class);
+
     private final EmployeeRepository employeeRepository;
     private final EmployeeGenerator employeeGenerator;
 
@@ -18,7 +25,42 @@ public class RunAtStart {
 
     @PostConstruct
     public void runAtStart() {
+        generateEmployees();
 
+        logger.info("\nSOME UNSORTED DATA\n");
+        printAll(findAllUnsorted());
+
+        logger.info("\nSOME SORTED DATA\n");
+        printAll(finAllSorted());
+
+        Page<Employee> pages = employeeRepository.findAll(new PageRequest(2, 10));
+        logger.info("TOTAL ELEMENTS:" + pages.getTotalElements());
+        logger.info("TOTAL PAGES:" + pages.getTotalPages());
+        logger.info("CURRENT PAGE: " + pages.getNumber());
+        printAll(pages.getContent());
     }
 
+    private void generateEmployees() {
+        for (int i=0; i<1000; i++) {
+            employeeRepository.save(employeeGenerator.generate());
+        }
+    }
+
+    private List<Employee> findAllUnsorted() {
+        return employeeRepository.findAll();
+    }
+
+    private List<Employee> finAllSorted() {
+        return employeeRepository.findAll(new Sort(
+                new Sort.Order(Sort.Direction.DESC, "firstName"),
+                new Sort.Order(Sort.Direction.ASC, "lastName")));
+    }
+
+    private void printAll(List<Employee> employees) {
+        employees.forEach(logger::info);
+
+        for (Employee employee: employees) {
+            logger.debug(employee.toString());
+        }
+    }
 }
